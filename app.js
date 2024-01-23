798dcd5cc4e379deea132809038667e817f364a1
safaley <safalneupane38@gmail.com>
2024-01-23 21:17:57 +0545

    last deploy

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const poolConnect = pool.connect();
 app.use(express.json());
 
 // Serve static files (including the HTML file)
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "html")));
 
 // Create 'tasks' table if not exists
 async function createTable() {
